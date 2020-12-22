5b7d71122816cc2e9ea3435bd13052471a03c5c9
ravnen707 <ravnen707@gmail.com>
2020-12-22 11:58:29 +0100

    Class for "bøf" and "Salat" To make arrayList for them

---
+++ src/sample/Salat.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Salat {
+    public int tal;
+    public  String ord;
+
+    public Salat(int tal, String ord) {
+        this.tal = tal;
+        this.ord = ord;
+    }
+
+    public int getTal() {
+        return tal;
+    }
+
+    public void setTal(int tal) {
+        this.tal = tal;
+    }
+
+    public String getOrd() {
+        return ord;
+    }
+
+    public void setOrd(String ord) {
+        this.ord = ord;
+    }
+}
