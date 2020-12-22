99d907a10f9d33678d32a2057145f3bbc4bc3156
ravnen707 <ravnen707@gmail.com>
2020-12-22 10:37:46 +0100

    KogebogGUI Added "Enter" on press Started a Switch

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.TextField;
+
+
+
 public class Controller {
+    public TextField SearchBar;
+
+    public void buttonOnAction() {
+    String search = SearchBar.getText();
+    int numbers = Integer.parseInt(search);
+    switch(numbers) {
+        case 1:
+            System.out.println("Pasta Carbonara"); // Viser det i Terminalen.
+            break;
+        case 2:
+            System.out.println("Bøf med løg");
+            break;
+        case 3:
+            System.out.println("Salat med Tun");
+            break;
+    }
+    }
+        public void searchbarOnAction(ActionEvent actionEvent) {
+            System.out.println("Enter"); // for terminalen
+        }
 }
